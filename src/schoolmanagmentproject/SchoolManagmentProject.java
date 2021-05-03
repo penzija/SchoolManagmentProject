b15ca3bda6df0ffd115d4515685d56ea81d25f1c
Luka Kik <luka.kik@iths.se>
2021-05-03 11:01:29 +0200

    First commit

---
+++ src/schoolmanagmentproject/SchoolManagmentProject.java
@@ -0,0 +1,33 @@
+package schoolmanagmentproject;
+
+public class SchoolManagmentProject {
+
+    public static void main(String[] args) {
+    }
+
+    private static void menu() {
+        
+//        Add educations of different kind
+//• Update education information
+//• Show information about a specific education
+//• Show all educations
+//• Remove an education
+//• Add courses of different kind
+//• Update course information
+//• Show information about a specific course
+//• Show all courses
+//• Remove a course
+//• Add students
+//• Update student information
+//• Show information about a specific student
+//• Show all students
+//• Remove a student
+//• Add courses to educations
+//• List all courses in an education
+//• remove courses from educations
+//• Add students to educations
+//• remove students from an education
+//• List all students in an education
+    }
+
+}
